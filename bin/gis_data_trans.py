a891d3074a6813f8f781e8d421df3a522bb7ecd9
like <328594663@qq.com>
2018-05-31 18:52:31 +0800

    v0.1

--- bin/gis_data_trans.py
+++ bin/gis_data_trans.py
@@ -96,7 +96,8 @@ def get_gis_cfg_data(dbname):
 def add_data_trans(mapping_tree, sourcedb_name, targetdb_name):
     sourcetable_name = mapping_tree.attrib['SOURCETABLE']
     targettable_name = mapping_tree.attrib['TARGETTABLE']
-    geometry_type=mapping_tree.attrib['GEOMETRYTYPE']
+    geometry_type = mapping_tree.attrib['GEOMETRYTYPE']
+    condition_name = mapping_tree.attrib['CONDITION']
     dir_seq, dir_srid = get_gis_cfg_data(targetdb_name)
     source_cols = []
     target_cols = []
@@ -114,34 +115,39 @@ def add_data_trans(mapping_tree, sourcedb_name, targetdb_name):
         # print(sourcecol_name, targetcol_name, rule_name)
     spilt_chr = ','
     source_line = spilt_chr.join(source_cols)
-    source_line = source_line+',BEFORE_AFTER,DEAL_DATE,OP_FLAG,DAL_FLAG'
+    source_line = source_line + ',BEFORE_AFTER,DEAL_DATE,OP_FLAG,DAL_FLAG'
     target_line = spilt_chr.join(target_cols)
-    get_data_sql = 'SELECT ' + source_line + ' FROM ' + sourcetable_name + ' WHERE DAL_FLAG IS NULL OR DAL_FLAG = 1 ORDER BY DEAL_DATE'
+    get_data_sql = 'SELECT ' + source_line + ' FROM ' + sourcetable_name + " WHERE BEFORE_AFTER = 'AFTER' AND (DAL_FLAG IS NULL OR DAL_FLAG = 1) ORDER BY DEAL_DATE"
     source_db_config = db_connect.get_db_config(db_cfg_file, sourcedb_name)
     source_db_conn = db_connect.get_connect(source_db_config)
     source_db_cursor = source_db_conn.cursor()
     source_db_cursor.execute(get_data_sql)
+    source_table_title = [i[0] for i in source_db_cursor.description]
     while 1:
         eachline = source_db_cursor.fetchone()
-        print(eachline)
         if eachline is None:
             break
         else:
             value = []
-            for index in range(len(eachline)-4):
+            for index in range(len(eachline) - 4):
                 if rule_list[index] == '':
-                    line_str = "'" + str(eachline[index]) + "'"
+                    if isinstance(eachline[index], datetime.datetime):
+                        line_str = "TO_DATE('" + str(eachline[index]) + "','YYYY-MM-DD hh24:mi:ss')"
+                    else:
+                        line_str = "'" + str(eachline[index]) + "'"
                 elif rule_list[index] == ':SEQ:':
                     line_str = dir_seq[targettable_name] + '.NEXTVAL'
                 elif rule_list[index] == ':TRANS_TYPE_ID:':
-                    line_str = "'" +trans_typeid(eachline[index])+ "'"
+                    line_str = "'" + trans_typeid(eachline[index]) + "'"
                 elif rule_list[index] == ':TRANS_RES_ID:':
-                    line_str = "'" +trans_res_id(eachline[index])+ "'"
-                elif rule_list[index]  == ':SHAPE:':
+                    line_str = "'" + trans_res_id(eachline[index]) + "'"
+                elif rule_list[index] == ':SHAPE:':
                     if geometry_type == '1':
-                        line_str = "sde.st_pointfromtext('"+eachline[index]+"',"+str(dir_srid[targettable_name])+')'
+                        line_str = "sde.st_pointfromtext('" + eachline[index] + "'," + str(
+                            dir_srid[targettable_name]) + ')'
                     elif geometry_type == '2':
-                        line_str = "sde.st_pointfromtext('" + eachline[index] + "'," + str(dir_srid[targettable_name]) + ')'
+                        line_str = "sde.st_pointfromtext('" + eachline[index] + "'," + str(
+                            dir_srid[targettable_name]) + ')'
                     else:
                         line_str = "'" + str(eachline[index]) + "'"
                 if line_str == "'None'":
@@ -149,10 +155,25 @@ def add_data_trans(mapping_tree, sourcedb_name, targetdb_name):
                 value.append(line_str)
             spilt_chr = ','
             line_str = spilt_chr.join(value)
-            print(line_str)
-
-    print(source_line)
-    print(target_line)
+            if eachline[-2] == 'INSERT':
+                final_sql = 'INSERT INTO ' + targettable_name + '(' + target_line + ') VALUES (' + line_str + ')'
+            elif eachline[-2] == 'UPDATE':
+                condition_index = re.search(r':(\w)+:', condition_name).span()
+                condition_id = condition_name[condition_index[0] + 1:condition_index[1] - 1]
+                condition_data_index = source_table_title.index(condition_id)
+                condition_data = eachline[condition_data_index]
+                condition_data = "'" + str(condition_data) + "'"
+                condition_final = re.sub(r':(\w)+:', condition_data, condition_name)
+                final_sql = 'UPDATE ' + targettable_name + ' SET (' + target_line + ')=( ' + line_str + ') WHERE ' + condition_final
+            elif eachline[-2] == 'DELETE':
+                condition_index = re.search(r':(\w)+:', condition_name).span()
+                condition_id = condition_name[condition_index[0] + 1:condition_index[1] - 1]
+                condition_data_index = source_table_title.index(condition_id)
+                condition_data = eachline[condition_data_index]
+                condition_data = "'" + str(condition_data) + "'"
+                condition_final = re.sub(r':(\w)+:', condition_data, condition_name)
+                final_sql = 'DELETE FROM ' + targettable_name + ' WHERE ' + condition_final
+            print(final_sql)
 
 
 if __name__ == '__main__':
